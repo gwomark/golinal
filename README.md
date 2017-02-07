3054262c6b78fee769e74f6e2f58c61215464e3a
hmc-cs-hawk <gwomark@cs.hmc.edu>
2017-02-07 00:49:41 -0800

    Initial Commit, started matrix tests.

---
+++ README.md
@@ -0,0 +1,4 @@
+This project is a Matrix data structure that provides the following functions:
+
+TODO: Fill stuff in here
+
---
+++ matrix_test.go
@@ -0,0 +1,101 @@
+package matrix
+
+import (
+	"testing";
+	"github.com/stretchr/testify/assert"
+    "github.com/stretchr/testify/suite"
+)
+
+
+//************************
+// Constructor Test Suite
+//************************
+
+type ConstructorsTestSuite struct {
+	suite.Suite
+	EmptyMatrix1 Matrix
+	EmptyMatrix2 Matrix
+	MatrixFromSlices1 Matrix
+	MatrixFromSlices2 Matrix 
+}
+
+
+type GetEntryTestSuite struct {}
+
+//********************************
+// Addition of Matrices Test Suite
+//********************************
+
+
+// This data structure is a test suite 
+// that tests different cases for adding Matrices. 
+type AdditionTestSuite struct {
+	suite.Suite
+	DiffDimMatrix1 Matrix
+	DiffDimMatrix2 Matrix
+	AddToItselfMatrix Matrix
+	ResultAddToItself Matrix
+	SquareMatrix1 Matrix
+	SquareMatrix2 Matrix
+
+}
+
+
+// Initializes all matrices to be tested 
+// in the addition test sweet. 
+func (suite *AdditionTestSuite) SetupTest() {
+	DiffDimMatrix1 := BlankMatrix{10, 2}
+	DiffDimMatrix1 := BlankMatrix{1, 5}
+
+	AddToItselfMatrix := Matrix{[]float64{1, 0, 0}, []float64{0, 1, 0}, []float64{0, 0, 1}}
+	ResultAddToItself := Matrix{[]float64{2, 0, 0}, []float64{0, 2, 0}, []float64{0, 0, 2}}
+
+	SquareMatrix1 := Matrix{[]float64{1, 6}, []float64{5, -7}}
+	SquareMatrix1 := Matrix{[]float64{1.05, -10}, []float64{-103, 4}}
+}
+
+
+// Different Dimension Addition Test
+// Adding two matrices of different dimensions should 
+// raise and Error
+func (suite *AdditionTestSuite) TestDifferentDimAddition() {
+	sum, err := suite.DiffDimMatrix1.Add(suite.DiffDimMatrix2)
+	assert.NotEqual(suite.T(), nil, err) 
+}
+
+func (suite *AdditionTestSuite) TestAddToItself() {
+	// Copy the matrix before we add it to itself
+	// Test this copy against the one that added to itself
+	// after adding
+	copyMatrix := suite.AddToItselfMatrix
+
+	sum        := suite.AddToItselfMatrix.Add(suite.AddToItselfMatrix)
+
+	assert.Equal(suite.T(), copyMatrix, suite.AddToItselfMatrix)
+	assert.Equal(suite.T(), suite.ResultAddToItself, sum)
+}
+
+type MultiplicationTestSuite struct{}
+
+type EqualsTestSuite struct{}
+
+
+type GetRowTestSuite struct {}
+
+type GetColTestSuite struct {}
+
+type DeterminantTestSuite struct {}
+
+type InverseTestSuite struct {}
+
+type GetEigenValuesTestSuite struct{}
+
+
+
+
+
+
+
+
+
+
