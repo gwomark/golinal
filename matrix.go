16a94bfd1c2dab08ba89424f8487cde049a15a44
hmc-cs-hawk <gwomark@cs.hmc.edu>
2017-04-27 12:30:40 -0700

    first push, shoulding've bee ndoing this a while ago

--- matrix.go
+++ matrix.go
@@ -1,3 +1,12 @@
+package matrix
+
+import (
+	"errors";
+	"math";
+	"fmt";
+	"github.com/gonum/matrix/mat64";
+)
+
 // Matrix Struct Definition
 
 type matrix struct {
@@ -16,14 +25,14 @@ type matrix struct {
 // the same length, otherwise the matrix will break
 //
 // returns: a pointer to a matrix
-func NewMatrix(slices... []float64) *matrix {
+func Matrix(slices... []float64) (*matrix) {
 	m := new(matrix)
-	m.numRows = len(elems)
-	m.numCols = len(elems[0])
+	m.numRows = int(len(slices))
+	m.numCols = int(len(slices[0]))
 	m.elems = slices
+
 	return m
 }
-
 
 
 // brief: Parameterized constructor that takes slice 
@@ -36,7 +45,8 @@ func NewMatrix(slices... []float64) *matrix {
 // the same length, otherwise the matrix will break
 //
 // returns: a pointer to a matrix
-func BlankMatrix(rows, cols int) *matrix {
+func BlankMatrix(rows, cols int) (*matrix) {
+
 	m := new(matrix)
 	m.numRows = rows
 	m.numCols = cols
@@ -50,3 +60,447 @@ func BlankMatrix(rows, cols int) *matrix {
 
 	return m
 }
+
+
+func Identity(n int) (*matrix) {
+
+	m := BlankMatrix(n, n)
+	for i := 0; i < n; i++ {
+		m.elems[i][i] = 1
+	}
+
+	return m
+}
+
+
+func (m matrix) Dims() (int, int) {
+	return m.numRows, m.numCols
+}
+
+
+// brief: Get the row,col'th entry of a matrix
+//
+// note: it is undefined behavior to use invalid indices with Get()
+//
+// returns: number of cols
+func (m matrix) At(row, col int) (float64) {
+
+	return m.elems[row][col]
+}
+
+
+// brief: Gets number of rows in a matrix
+//
+// returns: number of rows
+func (m matrix) NumRows() int {
+	return m.numRows
+}
+
+
+// brief: Gets number of cols in a matrix
+//
+// returns: number of cols
+func (m matrix) NumCols() int {
+	return m.numCols
+}
+
+
+// brief: Adds two matrices together
+// 
+// inputs: a matrix pointer
+//
+// returns: an error if dimensions of the 
+//          matrices to be summed aren't equal
+func (m *matrix) Add(q *matrix) error {
+	if (m.numRows != q.numRows) || (m.numCols != q.numRows) {
+		return errors.New("Dimensions aren't equal")
+	} else {
+		// Loop through each entry, store sum of entries in m
+		for i := 0; i < m.numRows; i++ {
+			for j := 0; j < q.numRows; j++ {
+				m.elems[i][j] += q.elems[i][j]
+			}
+		}
+		return nil
+	}
+}
+
+
+// brief: Scales a matrix by a real number
+// 
+// inputs: A float 
+func (m *matrix) Scale(x float64) {
+	for i := 0; i < m.numRows; i++ {
+			for j := 0; j < m.numRows; j++ {
+				m.elems[i][j] *= x
+			}
+		}	
+}
+
+
+// brief: Multiplys the matrix m by the matrix q
+//
+// details: O(n^3)
+// 
+// returns: product of m and q
+func (m matrix) Multiply(q *matrix) (*matrix, error) {
+
+	if (m.numCols != q.numRows) {
+		return nil,errors.New("Dimensions can't be multiplied")
+	} else {
+		result := BlankMatrix(m.numRows, q.numCols)
+		for i := 0; i < m.numRows; i++ {
+			for j := 0; j < q.numCols; j++ {
+				for k := 0; k < q.numRows; k++ {
+					result.elems[i][j] += m.elems[i][k] * q.elems[k][j]
+				}
+			}
+		}
+
+		return result, nil	
+	}
+}
+
+// brief: Calculates transpose of matrix, wrapper for T()
+//
+// details: Implemented for *matrix
+// 
+// returns: a transposed version of m as a pointer to a matrix
+
+func(m *matrix) Transpose() *matrix {
+	transpose := BlankMatrix(m.numCols, m.numRows)
+
+	for i := 0; i < m.numCols; i++ {
+		for j := 0; j < m.numRows; j++ {
+			fmt.Printf("i= %d\n", i)
+			fmt.Printf("j= %d\n", j)
+			transpose.elems[i][j] = m.elems[j][i]
+		}
+	}
+
+	return transpose
+}
+
+
+
+// brief: Calculates transpose of matrix
+//
+// details: Implemented for mat64.Matrix interface
+// 
+// returns: a transposed version of m
+func(m *matrix) Inverse() (*matrix, error) {
+	if !m.IsSqaure(){
+		return nil, errors.New("Matrix is not square")
+	}
+
+	inverseT := BlankMatrix(m.numRows,m.numRows)
+	identity := Identity(m.numRows)
+
+	for i, _ := range inverseT.elems {
+		r, err := m.Gauss(identity.elems[i])
+		if err != nil {
+			return nil, err
+		}
+
+		fmt.Println(r)
+		inverseT.elems[i] = r
+		
+	}
+
+	fmt.Println(inverseT.elems)
+
+	return inverseT, nil
+}
+
+
+// brief: Calculates determinant of a matrix
+//
+// details: Uses LU decomposition, O(n^3)
+// 
+// returns: a determinant of m
+func (m *matrix) Determinant() (det float64, err error) {
+	L, U, P, err := m.LUP()
+	if err != nil {
+		return 0.0, err
+	}
+
+	det, err = determinant(*L, *U, *P)
+	return
+}
+
+
+
+// brief: Calculates the LUP decomposition of a matrix
+//
+// details: Decomposes m in to the product of three matrices:
+//              P: A row permutation matrix
+//              U: An upper triangular matrix
+//              L: A lower triangular matrix
+//          Hence Pm = LU,
+//          O(n^3)
+//
+// returns: a determinant of m
+func (m *matrix) LUP() (*matrix, *matrix, *matrix, error) {
+	
+	// No LUP if matrix isn't square
+	if !m.IsSqaure() {
+		return nil, nil, nil, errors.New("LUP requires square matrix")
+	}
+
+	n := m.numRows
+	L := BlankMatrix(n,n)
+	U := BlankMatrix(n,n)
+	P := m.pivotMatrix()
+	Pm, _ := P.Multiply(m)
+
+	
+
+	for j := 0; j < n; j++ {
+		
+		L.elems[j][j] = 1
+
+		// Populate U using the following formula (compile it in LaTex)
+		// u_{ij} = a_{ij} - \sum_{k=1}^{i-1} u_{kj} l_{ik} 
+		for i := 0; i < j+1; i++ {
+			sum := 0.0
+			for k := 0; k < i; k++ {
+				sum += (U.elems[k][j] * L.elems[i][k])
+			}
+			
+			U.elems[i][j] = Pm.elems[i][j] - sum	
+
+		}
+		
+		
+		// Populate L using the following formula (compile it in LaTex)
+		// l_{ij} = \frac{1}{u_{jj}} (a_{ij} - \sum_{k=1}^{j-1} u_{kj} l_{ik}
+		for i := j; i < n; i++ {
+			sum := 0.0
+
+			for k := 0; k < j; k++ {
+				sum += (U.elems[k][j] * L.elems[i][k])
+			}
+			L.elems[i][j] = (Pm.elems[i][j] - sum) / U.elems[j][j]
+		}
+	}
+
+	return L, U, P, nil
+
+}
+
+
+func (m *matrix) RREF() *matrix {
+	
+	// loop through each row
+	for i := 0; i < m.numRows; i++ {
+		
+		for j := 0; j < m.numCols; j++ {
+
+		}
+
+	}
+	return BlankMatrix(4, 4)
+
+}
+
+
+
+// brief: Solves equations of the form
+//       Ax = b
+// where A is a matrix, and x,b are vectors
+//
+// inputs: b a slice of floats to solve for
+//
+// details: If A is square, LU decomposition is used
+//
+func (A *matrix) Gauss(b []float64) ([]float64, error) {
+	
+	
+	L, U, P, err := A.LUP()
+	if err != nil {
+		return nil, err
+	}
+
+	return gauss(b, L, U, P)
+
+}
+
+
+// brief: Finds the eigenvalues of a square matrix m
+//      
+//
+// returns: a slice of complex numbers
+func (m *matrix) Eigenvalues() ([]complex128, error) {
+	if m.IsSqaure() == false {
+		err := errors.New("Matrix should be square")
+		return nil, err
+	}
+
+	// Create an Eigen type 
+	var eigen mat64.Eigen
+
+	// Perform eigenvalue decomposition
+	eigen.Factorize(*m, true)
+	
+	return eigen.Values(nil), nil	
+}
+
+///////////////////////////////
+//         HELPER            //
+//         FUNCTIONS         //  
+///////////////////////////////
+
+
+func (m *matrix) IsSqaure() bool {
+	return (m.numRows == m.numCols)
+}
+
+
+
+// brief: Calculates transpose of matrix
+//
+// details: Implemented for mat64.Matrix interface
+// 
+// returns: a transposed version of m
+func (m matrix) pivotMatrix() *matrix { 
+
+	dim := m.numRows
+
+	// Permutation matrix to return
+	P := Identity(dim)
+	
+	// For each column, find the row below the diagonal
+	// with the highest entry in that column
+	for j, row := range m.elems {
+		currentMax := row[j]
+		maxRow := j
+
+		
+		for i := j; i < dim; i++ {
+			if m.elems[i][j] > currentMax {
+				currentMax = m.elems[i][j]
+				maxRow = i
+			}
+		}
+
+		// If the j'th entry in maxRow is 
+		// not on the diagonal, swap the
+		// corresponding rows in P
+		if maxRow != j {
+			P.elems[j], P.elems[maxRow] = P.elems[maxRow], P.elems[j]
+		}
+	}
+
+	return P
+}
+
+
+func permDeterminant(m *matrix) float64 {
+	numberOfSwitches := 0.0
+	
+	for i := 0; i < m.numRows; i++ {
+		if m.elems[i][i] != 1 {
+			numberOfSwitches += 1
+		}
+	}
+
+	return math.Pow(-1.0, (numberOfSwitches / 2.0))
+}
+
+
+func determinant(L, U, P matrix) (float64, error) {
+	n := L.numRows
+	
+	// det(m) = det(L)det(P^-1)det(U)
+	detL := 1.0
+	detU := 1.0
+	detP := permDeterminant(&P)
+
+	// Multiply diagonals to calculate det(L) and det(U)
+	for i := 0; i < n; i++ {
+		detL = L.elems[i][i] * detL
+	}
+
+	for i := 0; i < n; i++ {
+		detU = U.elems[i][i] * detU
+	}
+
+	return (detL * detU * detP), nil
+
+}
+
+
+func gauss(b []float64, L *matrix, U *matrix, P *matrix) ([]float64, error) {
+	
+	// Multiply P 
+	Pb, err := P.Multiply(Matrix(b).Transpose())
+
+	
+	if err != nil {
+		return nil, err
+	}
+
+	// Solve Ly = Pb
+	y := make([]float64, len(b))
+	for i, row := range L.elems {
+		sum := 0.0
+		for j := 0; j < i; j++ {
+			sum += row[j]*y[j]
+		}
+
+		y[i] = (Pb.elems[i][0] - sum) / row[i]
+	 
+	}
+
+	//fmt.Println(b)
+
+	// Solve Ux = y 
+	// Perform backwards substitution
+	x := make([]float64, len(b))
+	for i := len(b)- 1; i >= 0; i++ {
+		sum := 0.0
+		for j := len(b)-1; j > i; j++ {
+			sum += U.elems[i][j]*x[j]
+		}
+
+		x[i] = (y[i] - sum) / U.elems[i][0]
+	}
+
+	fmt.Println(x)
+	return x, nil
+}
+
+// brief: Finds the max entry in a 
+//        slice of floats and it index
+//
+// returns: the max value in the slice and its index
+func Max(s []float64) (float64, int) {
+	n:= len(s)
+	currentMax := s[0]
+	index := 0
+
+	for i := 1; i < n; i++ {
+		if s[i] > currentMax {
+			currentMax = s[i]
+			index = i
+		}
+	} 
+
+	return currentMax, index
+}
+
+// brief: Calculates transpose of matrix
+//
+// details: Implemented for mat64.Matrix interface
+// 
+// returns: a transposed version of m
+func (m matrix) T() mat64.Matrix {
+	transpose := m.Transpose()
+
+
+	return *transpose
+
+}
+
+
+
--- matrix_test.go
+++ matrix_test.go
@@ -1,10 +1,42 @@
 package matrix
 
 import (
-	"testing";
-	"github.com/stretchr/testify/assert"
-    "github.com/stretchr/testify/suite"
+    "github.com/stretchr/testify/suite";
+    "testing"
 )
+
+//****************
+// Global Matrices
+//****************
+
+var NonsquareMatrix = Matrix([]float64{1}, []float64{-7})
+var NonsquareMatrix2 = Matrix([]float64{3, 4})
+
+var ThreeIdentity = Matrix([]float64{1, 0, 0}, []float64{0, 1, 0}, []float64{0, 0, 1})
+
+var RandFourMatrix = Matrix (
+        []float64{0.223548, 7.51484, 7.94393, 7.95676}, 
+        []float64{9.44692, -2.05097, -3.59421, -7.9301}, 
+        []float64{-5.90911, -9.56427, -6.67171, -8.09466}, 
+        []float64{-9.43214, 6.42982, 7.37722, 3.8219})
+
+
+
+var RandMatrix = Matrix(
+        []float64{2.99875, -0.722266, -0.237451, -1.11405, -2.127, 8.88714, -1.65288, -5.27189, -5.92509, -6.02403}, 
+        []float64{3.26164, 5.86218, 2.81815, -2.06958, 0.366388, -0.271817, -3.51731, 3.22294, -4.71693, -8.95407}, 
+        []float64{6.53936, 0.653704, 5.51595, 8.75519, 4.50956, -2.18589, 1.44052, -7.2319, -6.35739, 9.8645}, 
+        []float64{-7.58145, 8.1194, -7.58264, 9.88342, 1.48929, -3.66263, 1.87859, 8.37529, 0.772604, -6.30053}, 
+        []float64{-9.39711, -6.49522, 1.94943, 9.03285, 3.40668, -7.61823, -7.2272, 4.2087, -8.91554, 4.15006}, 
+        []float64{-2.80846, 5.17557, 9.52006, -9.47033, 5.67815, 6.8402, 0.818774, -6.92541, 8.22727, 5.83063}, 
+        []float64{0.627448, -3.28157, 5.50732, 9.96776, -7.11846, -7.3921, -6.67718, -0.621603, -7.81631, -2.35664}, 
+        []float64{-3.93259, -2.36996, 4.96011, 7.4244, -6.90362, -1.08256, 5.21275, -8.66966, -8.88118, -3.51107}, 
+        []float64{8.40158, -6.03886,  7.62759, -2.43493, 0.36834, 9.46535, -4.76645, 7.49544, -7.4696, -7.31169}, 
+        []float64{-5.5276, -5.28959, -2.90794, -0.984676, 7.66101, -4.13466, -6.00181, 1.37331, -9.62865, 1.92439})
+
+
+// Nil pointer for matrix
+var nilMatrixP *matrix
 
 
 //************************
@@ -13,17 +45,26 @@ import (
 
 type ConstructorsTestSuite struct {
 	suite.Suite
-	EmptyMatrix1 Matrix
-	EmptyMatrix2 Matrix
-	MatrixFromSlices1 Matrix
-	MatrixFromSlices2 Matrix 
+
+	EmptyMatrix, 
+	MatrixFromSlices *matrix 
 }
 
-//************************
-// END Constructor Test Suite
-//************************
 
-type GetEntryTestSuite struct {}
+func (suite *ConstructorsTestSuite) SetupTest() {
+    suite.EmptyMatrix = BlankMatrix(2, 3)
+
+    suite.MatrixFromSlices = Matrix([]float64{1, 0, 0}, []float64{0, 1, 0}, []float64{0, 0, 1})
+}
+
+func (suite *ConstructorsTestSuite) TestConstructors() {
+    suite.Equal(suite.EmptyMatrix.NumRows(), 2, "They should be equal")
+    suite.Equal(suite.EmptyMatrix.NumCols(), 3, "They Should be equal")
+
+    suite.Equal(suite.MatrixFromSlices.NumRows(), 3, "They should be equal")
+    suite.Equal(suite.MatrixFromSlices.NumCols(), 3, "They Should be equal")
+}
+
 
 //********************************
 // Addition of Matrices Test Suite
@@ -34,32 +75,35 @@ type GetEntryTestSuite struct {}
 // that tests different cases for adding Matrices. 
 type AdditionTestSuite struct {
 	suite.Suite
-	DiffDimMatrix1 Matrix
-	DiffDimMatrix2 Matrix
-	AddToItselfMatrix Matrix
-	ResultAddToItself Matrix
-	SquareMatrix1 Matrix
-	SquareMatrix2 Matrix
+
+	DiffDimMatrix1,
+	DiffDimMatrix2 *matrix
+
+	AddToItselfMatrix,
+    CopyOfItselfMatrix,
+	ResultAddToItself *matrix
+
+	SquareMatrix1,
+	SquareMatrix2,
+    ResultMatrix *matrix
 
 }
-
-//********************************
-// END Addition of Matrices Test Suite
-//********************************
-
-type EqualsTestSuite struct{}
 
 // Initializes all matrices to be tested 
 // in the addition test sweet. 
 func (suite *AdditionTestSuite) SetupTest() {
-	DiffDimMatrix1 := BlankMatrix{10, 2}
-	DiffDimMatrix1 := BlankMatrix{1, 5}
+    suite.DiffDimMatrix1 = BlankMatrix(10, 2)
+    suite.DiffDimMatrix2 = BlankMatrix(1, 5)
 
-	AddToItselfMatrix := Matrix{[]float64{1, 0, 0}, []float64{0, 1, 0}, []float64{0, 0, 1}}
-	ResultAddToItself := Matrix{[]float64{2, 0, 0}, []float64{0, 2, 0}, []float64{0, 0, 2}}
+    
+    suite.AddToItselfMatrix = Matrix([]float64{1, 0, 0}, []float64{0, 1, 0}, []float64{0, 0, 1})
+    suite.CopyOfItselfMatrix = ThreeIdentity
+    suite.ResultAddToItself = Matrix([]float64{2, 0, 0}, []float64{0, 2, 0}, []float64{0, 0, 2})
 
-	SquareMatrix1 := Matrix{[]float64{1, 6}, []float64{5, -7}}
-	SquareMatrix1 := Matrix{[]float64{1.05, -10}, []float64{-103, 4}}
+    
+    suite.SquareMatrix1 = Matrix([]float64{1, 6}, []float64{5, -7})
+    suite.SquareMatrix2 = Matrix([]float64{1.05, -10}, []float64{-103, 4})
+    suite.ResultMatrix  = Matrix([]float64{2.05, -4}, []float64{-98, -3})
 }
 
 
@@ -67,8 +111,8 @@ func (suite *AdditionTestSuite) SetupTest() {
 // Adding two matrices of different dimensions should 
 // raise and Error
 func (suite *AdditionTestSuite) TestDifferentDimAddition() {
-	sum, err := suite.DiffDimMatrix1.Add(suite.DiffDimMatrix2)
-	assert.NotEqual(suite.T(), nil, err) 
+    err := suite.DiffDimMatrix1.Add(suite.DiffDimMatrix2)
+    suite.NotEqual(nil, err)
 }
 
 
@@ -77,41 +121,330 @@ func (suite *AdditionTestSuite) TestDifferentDimAddition() {
 // This tests to make sure that we don't modify the original
 // arguments when we add a matrix to itself
 func (suite *AdditionTestSuite) TestAddToItself() {
-	// Copy the matrix before we add it to itself
-	// Test this copy against the one that added to itself
-	// after adding
-	copyMatrix := suite.AddToItselfMatrix
+    // Copy the matrix before we add it to itself
+    // Test this copy against the one that added to itself
+    // after adding
+    
 
-	sum        := suite.AddToItselfMatrix.Add(suite.AddToItselfMatrix)
+    err := suite.AddToItselfMatrix.Add(suite.AddToItselfMatrix)
 
-	assert.Equal(suite.T(), copyMatrix, suite.AddToItselfMatrix)
-	assert.Equal(suite.T(), suite.ResultAddToItself, sum)
+    suite.Equal(err, nil, "They should be equal")
+    suite.NotEqual(suite.CopyOfItselfMatrix, suite.AddToItselfMatrix, "They should not be equal")
+    suite.Equal(suite.ResultAddToItself, suite.AddToItselfMatrix, "They should be equal")
+}
+
+func (suite *AdditionTestSuite) TestSimpleAdd() {
+    err := suite.SquareMatrix1.Add(suite.SquareMatrix2)
+
+    suite.Equal(err, nil, "They should be equal")
+    suite.Equal(suite.ResultMatrix, suite.SquareMatrix1, "They should be equal")
+
 }
 
 
-
-//********************************
+//**************************************
 // Multiplication of Matrices Test Suite
-//********************************
+//**************************************
 
-type MultiplicationTestSuite struct{
+type MultiplicationTestSuite struct {
+    suite.Suite
+    MismatchRowCol1,
+    MismatchRowCol2,
+    SquaredRandMatrix *matrix
+    
+}
 
+
+func (suite *MultiplicationTestSuite) SetupTest() {
+    suite.MismatchRowCol1 = Matrix([]float64{1, 6, 3}, []float64{5, -7, 3})
+    suite.MismatchRowCol2 = NonsquareMatrix
+
+    suite.SquaredRandMatrix = Matrix(
+        []float64{8.9925, 0.521668, 0.056383, 1.24111, 4.52413, 78.9813, 2.73201, 27.7928, 35.1067, 36.2889}, 
+        []float64{10.6383, 34.3652, 7.94197, 4.28316, 0.13424, 0.0738845, 12.3715, 10.3873, 22.2494, 80.1754}, 
+        []float64{42.7632, 0.427329, 30.4257, 76.6534, 20.3361, 4.77812, 2.0751, 52.3004, 40.4164, 97.3084}, 
+        []float64{57.4784, 65.9247, 57.4964, 97.682, 2.21798, 13.4149, 3.5291, 70.1455, 0.596917, 39.6967}, 
+        []float64{88.3057, 42.1879, 3.80028, 81.5924, 11.6055, 58.0374, 52.2324, 17.7132, 79.4869, 17.223}, 
+        []float64{7.88745, 26.7865, 90.6315, 89.6872, 32.2414, 46.7883, 0.670391, 47.9613, 67.688, 33.9962}, 
+        []float64{0.393691, 10.7687, 30.3306, 99.3562, 50.6725, 54.6431, 44.5847, 0.38639, 61.0947, 5.55375}, 
+        []float64{15.4653, 5.61671, 24.6027, 55.1217, 47.66, 1.17194, 27.1728, 75.163, 78.8754, 12.3276}, 
+        []float64{70.5865, 36.4678, 58.1801, 5.92888, 0.135674, 89.5929, 22.719, 56.1816, 55.7949, 53.4608}, 
+        []float64{30.5544, 27.9798, 8.45612, 0.969587, 58.6911, 17.0954, 36.0217, 1.88598, 92.7109, 3.70328})
+}
+
+func (suite *MultiplicationTestSuite) TestMultiplication() {
+    mult1, err1 := suite.MismatchRowCol1.Multiply(suite.MismatchRowCol2)
+    mult2, err2 := suite.MismatchRowCol2.Multiply(suite.MismatchRowCol1)
+    mult3, err3 := ThreeIdentity.Multiply(ThreeIdentity)
+    mult4, err4 := RandMatrix.Multiply(Identity(10))
+    mult5, err5 := RandMatrix.Multiply(RandMatrix)
+
+    suite.Equal(mult1, nilMatrixP, "They should be equal")
+    suite.NotEqual(err1, nil, "They should not be equal")
+
+    suite.Equal(mult2, nilMatrixP, "They should be equal")
+    suite.NotEqual(err2, nil, "They should be equal")
+
+    suite.Equal(mult3, ThreeIdentity, "They should be equal")
+    suite.Equal(err3, nil, "They should be equal")
+
+    suite.Equal(RandMatrix, mult4, "They should be equal")
+    suite.Equal(err4, nil, "They should be equal")
+
+    suite.Equal(suite.SquaredRandMatrix, mult5, "They should be equal")
+    suite.Equal(err5, nil, "They should be equal")
+}
+
+
+//*****************************
+// LUP Decomposition Test Suite
+//*****************************
+
+type LUPDecompTestSuite struct {
+    suite.Suite
+
+    ThreeMatrix,
+    FourMatrix *matrix
+
+    LThree,
+    UThree,
+    PThree *matrix
+
+    LFour,
+    UFour,
+    PFour *matrix
+}
+
+func (suite *LUPDecompTestSuite) SetupTest() {
+    suite.ThreeMatrix = Matrix(
+        []float64{1,  3,  5},
+        []float64{2,  4,  7},
+        []float64{1,  1,  0})
+
+    suite.LThree = Matrix(
+        []float64{1.00000,  0.00000,  0.00000},
+        []float64{0.50000,  1.00000,  0.00000},
+        []float64{0.50000, -1.00000,  1.00000})
+
+    suite.UThree = Matrix(
+        []float64{2.00000,  4.00000,  7.00000},
+        []float64{0.00000,  1.00000,  1.50000},
+        []float64{0.00000,  0.00000, -2.00000})
+
+    suite.PThree = Matrix(
+        []float64{0,  1,  0},
+        []float64{1,  0,  0},
+        []float64{0,  0,  1})
+
+    suite.FourMatrix = Matrix(
+        []float64{11,  9, 24,  2},
+        []float64{ 1,  5,  2,  6},
+        []float64{ 3, 17, 18,  1},
+        []float64{ 2,  5,  7,  1})
+
+    suite.LFour = Matrix(
+        []float64{1.00000,  0.00000, 0.00000,  0.00000},
+        []float64{0.27273,  1.00000, 0.00000,  0.00000},
+        []float64{0.09091,  0.28750, 1.00000,  0.00000},
+        []float64{0.18182,  0.23125, 0.00360,  1.00000})
+
+    suite.UFour = Matrix(
+        []float64{1.00000,  0.00000, 0.00000,  0.00000},
+        []float64{0.27273,  1.00000, 0.00000,  0.00000},
+        []float64{0.09091,  0.28750, 1.00000,  0.00000},
+        []float64{0.18182,  0.23125, 0.00360,  1.00000})
+    
+    suite.PFour = Matrix(
+        []float64{1, 0, 0, 0},
+        []float64{0, 0, 1, 0},
+        []float64{0, 1, 0, 0},
+        []float64{0, 0, 0, 1})
+}
+
+func (suite *LUPDecompTestSuite) TestLUP() {
+    L1, U1, P1, err1 := suite.ThreeMatrix.LUP()
+    L2, U2, P2, err2 := suite.FourMatrix.LUP()
+
+    suite.Equal(suite.LThree, L1, "They should be equal")
+    suite.Equal(suite.UThree, U1, "They should be equal")
+    suite.Equal(suite.PThree, P1, "They should be equal")
+    suite.Equal(nil, err1, "There should be no error")
+
+    suite.Equal(suite.LFour, L2, "They should be equal")
+    suite.Equal(suite.UFour, U2, "They should be equal")
+    suite.Equal(suite.PFour, P2, "They should be equal")
+    suite.Equal(nil, err2, "There should be no error")
+   
+}
+
+
+//*******************************
+// Inverse of Matrices Test Suite
+//*******************************
+type InverseTestSuite struct {
+    suite.Suite
+
+    ZeroDeterminantMatrix,
+    TenIdentity *matrix
+
+    RandMatrixInverse *matrix
+}
+
+func (suite *InverseTestSuite) SetupTest() {
+
+
+    suite.ZeroDeterminantMatrix = Matrix([]float64{2, -2}, []float64{-2, 2})
+
+    suite.TenIdentity = Identity(10)
+
+    suite.RandMatrixInverse = Matrix(
+        []float64{-0.0460854, -0.0705092, 0.0326931, 0.045097, -0.164044, 0.0123061, 0.104152, -0.0198876, 0.0493236, 0.102875}, 
+        []float64{0.141227, 0.325278, 0.0262483, -0.179546, 0.419574, -0.121419, -0.306189, -0.0357916, -0.162755, -0.362409}, 
+        []float64{-0.0715951, -0.0268494, 0.0000621608, 0.029943, -0.0600711, 0.0617097, 0.0618292, 0.0345928, 0.0558978, 0.0424593}, 
+        []float64{-0.0430982, -0.202563, 0.0235125, 0.164038, -0.235156, 0.0869331, 0.207031, -0.00181176, 0.0909108, 0.178495}, 
+        []float64{-0.170888, -0.341571, 0.0211468, 0.281072, -0.546889, 0.193541, 0.36861, 0.0233994, 0.187385, 0.486657}, 
+        []float64{0.133272, 0.0910483, 0.00621968, -0.0741133, 0.234174, -0.0504633, -0.16888, -0.0265523, -0.0423938, -0.202145}, 
+        []float64{-0.0699641, 0.0156878, 0.0117219, -0.00723369, -0.00487834, -0.029769, -0.0838612, 0.0756502, 0.0214575, -0.0122182}, 
+        []float64{0.0507206, 0.222235, -0.00168086, -0.150038, 0.356268, -0.126108, -0.272279, -0.0189743, -0.0668899, -0.298225}, 
+        []float64{-0.133813, -0.329467, -0.0227785, 0.228099, -0.457624, 0.170425, 0.362524,-0.00890932, 0.141323, 0.346878}, 
+        []float64{0.168294, 0.34461, 0.031145, -0.287184, 0.592122, -0.189706, -0.426741, -0.0494601, -0.190007, -0.486913})
+}
+
+func (suite *InverseTestSuite) TestInverse() {
+    inverse1, err1 := NonsquareMatrix.Inverse()
+    inverse2, err2 := suite.ZeroDeterminantMatrix.Inverse()
+    inverse3, err3 := suite.TenIdentity.Inverse()
+    inverse4, err4 := RandMatrix.Inverse()
+
+
+    suite.Equal(nilMatrixP, inverse1, "Non sqaure matrix should be nil")
+    suite.NotEqual(nil, err1, "There should be an error")
+
+    suite.Equal(nilMatrixP, inverse2, "Zero determinant should be nil")
+    suite.NotEqual(nil, err2, "There should be an error")
+
+    suite.Equal(suite.TenIdentity, inverse3, "Identity inverse is itself")
+    suite.Equal(nil, err3, "There should be no error")
+
+    suite.Equal(suite.RandMatrixInverse, inverse4, "Identity inverse is itself")
+    suite.Equal(nil, err4, "There should be no error")
 }
 
 
 
-type GetRowTestSuite struct {}
 
-type GetColTestSuite struct {}
+//****************************************************
+// Eigenvalues and Determinants of Matrices Test Suite
+//****************************************************
+type EigValDeterminantTestSuite struct{
+    suite.Suite
 
-type DeterminantTestSuite struct {}
+    Uppertriangular1,
+    Uppertriangular2, 
+    RandMatrix *matrix
 
-type InverseTestSuite struct {}
+    IdentityEigenVals,
+    Upper1EigenVals,
+    Upper2EigenVals,
+    RandEigenVals []complex128
 
-type GetEigenValuesTestSuite struct{}
+}
 
 
+func (suite *EigValDeterminantTestSuite) SetupTest() {
 
+    suite.IdentityEigenVals = []complex128{1+0i, 1+0i, 1+0i, 1+0i, 1+0i}
+
+    suite.Uppertriangular1 = Matrix(
+        []float64{5, 10, 9, 3, 4}, 
+        []float64{0, 4, -6, 7.234, -3}, 
+        []float64{0, 0, 3, 13098.38, 239}, [
+        ]float64{0, 0, 0, 2, -70}, 
+        []float64{0, 0, 0, 0, 1})
+
+    suite.Upper1EigenVals  = []complex128{5+0i, 4+0i, 3+0i, 2+0i, 1+0i}
+
+    suite.Uppertriangular2 = Matrix(
+        []float64{1, 10, 9, 3, 4}, 
+        []float64{0, 3, -6, 7.234, -3}, 
+        []float64{0, 0, 4, 13098.38, 239}, [
+        ]float64{0, 0, 0, 5, -70}, 
+        []float64{0, 0, 0, 0, 2})
+
+    suite.Upper2EigenVals = []complex128{1+0i, 3+0i, 4+0i, 5+0i, 2+0i}
+
+    suite.RandEigenVals = []complex128{18.5377+11.1238i, 18.5377-11.1238i, -13.4118+9.58048i, -13.4118-9.58048i, -13.5186+0i, 0.557657+12.1091i, 0.557657-12.1091i, 9.66163+0i, 7.42975+0i, -1.32488+0i}
+}
+
+
+func (suite *EigValDeterminantTestSuite) TestDeterminant() {
+    det1, err1 := NonsquareMatrix.Determinant()
+
+    det2, err2 := ThreeIdentity.Determinant()
+    det3, err3 := suite.Uppertriangular1.Determinant()
+    det4, err4 := suite.Uppertriangular2.Determinant()
+    det5, err5 := RandMatrix.Determinant()
+    det6, err6 := RandFourMatrix.Determinant()
+
+    suite.Equal(0.0, det1, "There should be no determinant")
+    suite.NotEqual(err1, nil, "There should be an error")
+
+    suite.Equal(det2, 1.0, "They should be equal")
+    suite.Equal(err2, nil, "There should be no error")
+
+    suite.Equal(det3, 120.0, "They should be equal")
+    suite.Equal(err3, nil, "There should be no error")
+    
+    suite.Equal(det4, 120.0,"They should be equal")
+    suite.Equal(err4, nil, "There should be no error")
+
+    suite.Equal(det5, 2.39872e+10, "They should be equal")
+    suite.Equal(err5, nil, "There should be no error")
+
+    suite.Equal(0.0, det6, "They should be equal")
+    suite.Equal(nil, err6, "There should be no error")
+
+}
+
+func (suite *EigValDeterminantTestSuite) TestEigenvalues() {
+    eval1, err1 := NonsquareMatrix.Eigenvalues()
+    eval2, err2 := ThreeIdentity.Eigenvalues()
+    eval3, err3 := suite.Uppertriangular1.Eigenvalues()
+    eval4, err4 := suite.Uppertriangular2.Eigenvalues()
+    eval5, err5 := RandMatrix.Eigenvalues()
+
+
+    suite.Equal(eval1, nil, "There should be no eigenvalues")
+    suite.NotEqual(err1, nil, "There should be an error")
+
+    suite.Equal(eval2, suite.IdentityEigenVals, "They should be equal")
+    suite.Equal(err2, nil, "There should be no error")
+
+    suite.Equal(eval3, suite.Upper1EigenVals, "They should be equal")
+    suite.Equal(err3, nil, "There should be no error")
+    
+    suite.Equal(eval4, suite.Upper2EigenVals,"They should be equal")
+    suite.Equal(err4, nil, "There should be no error")
+
+    suite.Equal(eval5, suite.RandEigenVals, "They should be equal")
+    suite.Equal(err5, nil, "There should be no error")
+
+}
+
+
+// brief: Runs all test suites when "go test" is run
+//
+// 
+//
+func TestAll(t *testing.T) {
+	suite.Run(t, new(ConstructorsTestSuite))
+    suite.Run(t, new(AdditionTestSuite))
+    suite.Run(t, new(MultiplicationTestSuite))
+    suite.Run(t, new(LUPDecompTestSuite))
+    suite.Run(t, new(InverseTestSuite))
+    suite.Run(t, new(EigValDeterminantTestSuite))
+    
+}
 
 
 
